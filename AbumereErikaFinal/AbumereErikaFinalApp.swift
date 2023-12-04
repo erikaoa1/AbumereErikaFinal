37546665bb99fad661cf11d809f962fc5802f793
Erika <erikaabumere@gmail.com>
2023-12-03 23:40:47 -0800

    first commit

---
+++ AbumereErikaFinal/AbumereErikaFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbumereErikaFinalApp.swift
+//  AbumereErikaFinal
+//
+//  Created by Student on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AbumereErikaFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
