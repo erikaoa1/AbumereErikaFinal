341d922830c4c4c0d1d77cfb459172e8537aaa2e
Erika <erikaabumere@gmail.com>
2023-12-05 17:43:20 -0800

    updated commit

--- AbumereErikaFinal/TranscribePage.swift
+++ AbumereErikaFinal/TranscribePage.swift
@@ -27,100 +27,31 @@ struct TranscribePage: View {
                     .frame(width: 300, height: 300)
                 
             }
+            
+            if transcriptionViewModel.isLoading {
+                //print("loading")
+                ProgressView()
+            } else {
+                //fully loaded and api has returned descriptinos json
+                
+                let descriptions = transcriptionViewModel.descriptions
+                                
+            }
         }
         .onChange(of: imageItem) { _ in
             Task {
                 if let data = try? await imageItem?.loadTransferable(type: Data.self) {
                     if let image = UIImage(data: data) {
                         //display the image
-                        selectedImage = Image(uiImage: image)
+                        //selectedImage = Image(uiImage: image)
                         
                         await transcriptionViewModel.getDescription(image: image)
-                        
-                        if transcriptionViewModel.isLoading {
-                            print("loading")
-                        } else {
-                            print("done loading")
-                            //fully loaded, api has returned descriptinos json
-                            
-                            let descriptions = transcriptionViewModel.descriptions
-                            
-                                print(descriptions)
-                        
-                            
-                            
-                            
-                            return
-                        }
+                        //if else block use to be here
                     }
-                    
                     print("Failed")
                 }
             }
         }
         
-        
-        
-        
-        
-        
-        /*
-         
-         struct TranscribePage_Previews: PreviewProvider {
-             static var previews: some View {
-                 TranscribePage()
-             }
-         }
-         
-         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
-         print("Failed to convert image to data")
-         return
-         }
-         
-         let apiKey = "E843F250-F8D2-45FA-AB61-C722E201A70882591561-BA9A-4859-AE59-F64CB368E550"
-         let apiUrl = "https://vision.astica.ai/describe"
-         let modelVersion = "2.0_full" // Use version 2.0
-         
-         // Define the parameters for the API request
-         let parameters: [String: Any] = [
-         "tkn": apiKey,
-         "modelVersion": modelVersion,
-         "visionParams": "description", // Specify the desired metadata (description/caption)
-         "input": imageData.base64EncodedString() // Use Base64 encoded string of the image
-         ]
-         
-         // Convert parameters to JSON data
-         guard let jsonBody = try? JSONSerialization.data(withJSONObject: parameters) else {
-         print("Failed to convert parameters to JSON data")
-         return
-         }
-         
-         
-         var request = URLRequest(url: URL(string: apiUrl)!)
-         request.httpMethod = "POST"
-         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-         request.httpBody = jsonBody
-         
-         URLSession.shared.dataTask(with: request) { data, response, error in
-         if let data = data {
-         do {
-         // Parse the JSON data
-         let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-         
-         // Extract the caption from the response
-         if let caption = json?["caption"] as? String {
-         print("Generated Caption: \(caption)")
-         } else {
-         print("Caption not found in the response.")
-         }
-         } catch {
-         print("Error parsing JSON: \(error.localizedDescription)")
-         }
-         } else if let error = error {
-         print("Error: \(error.localizedDescription)")
-         }
-         }.resume()
-         }
-         */
     }
 }
--- AbumereErikaFinal/TranscriptionViewModel.swift
+++ AbumereErikaFinal/TranscriptionViewModel.swift
@@ -12,7 +12,11 @@ import UIKit
 class TranscriptionViewModel: ObservableObject {
     private let url = "https://vision.astica.ai/describe"
     private let apiKey = "E843F250-F8D2-45FA-AB61-C722E201A70882591561-BA9A-4859-AE59-F64CB368E550"
-    let params = "describe,gpt,gpt_detailed"
+    let params = "gpt"
+    //works for describe
+    //works for gpt
+    // works for gpt_detailed
+    //seems to only work with one param
     
     
 
@@ -31,17 +35,34 @@ class TranscriptionViewModel: ObservableObject {
             isLoading = true
 
             var request = URLRequest(url: URL(string: url)!)
-            //request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue(apiKey, forHTTPHeaderField: "tkn")
-            request.setValue("2.0_full", forHTTPHeaderField: "modelVersion")
-            request.setValue(params, forHTTPHeaderField: "visionParams")
-            request.setValue(imageAsString, forHTTPHeaderField: "input")
-
-
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            //request.setValue(apiKey, forHTTPHeaderField: "tkn")
+            //request.setValue("2.0_full", forHTTPHeaderField: "modelVersion")
+            //request.setValue(params, forHTTPHeaderField: "visionParams")
+            //request.setValue(imageAsString, forHTTPHeaderField: "input")
+            
+            let parameters = [
+                "tkn": apiKey,
+                "input": imageAsString,
+                "visionParams": params,
+                "modelVersion": "2.0_full"
+            
+            ]
+            /*
+            request.httpBody = try JSONSerialization.data(withJSONObject: ["tkn": apiKey])
+            request.httpBody = try JSONSerialization.data(withJSONObject: ["input": imageAsString])
+            request.httpBody = try JSONSerialization.data(withJSONObject: ["visionParams": params])
+            request.httpBody = try JSONSerialization.data(withJSONObject: ["modelVersion": "2.0_full"])
+*/
+            let requestJSON = try? JSONSerialization.data(withJSONObject: parameters)
+            request.httpBody = requestJSON
+            
             let (data, _) = try await URLSession.shared.data(for: request)
             
             //print raw json data
-            print(String(data: data, encoding: .utf8) ?? "Data is not valid UTF-8")
+            //print(String(data: data, encoding: .utf8) ?? "Data is not valid UTF-8")
+            
             //let decoder = JSONDecoder()
             //let results = try decoder.decode(String.self, from: data)
             
